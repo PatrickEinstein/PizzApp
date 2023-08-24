05779d86c589c0d154005d6dcf6075bf4f5f168c
PatrickEinstein <mohammedola1234@gmail.com>
2023-08-24 23:53:56 +0100

    now working again

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ReactDOM } from "react";
-// import { Stack } from "expo-router";
 import { useCallback } from "react";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
